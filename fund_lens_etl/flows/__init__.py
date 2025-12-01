88054f4a4201f1afeae4674f08ce96877306c76b
Tom Boone <tbc@tomboone.com>
2025-12-01 12:12:15 -0500

    add maryland contribution data

--- fund_lens_etl/flows/__init__.py
+++ fund_lens_etl/flows/__init__.py
@@ -1,6 +1,9 @@
 """Prefect flows for ETL pipeline orchestration."""
 
 from fund_lens_etl.flows.bronze_ingestion_flow import bronze_ingestion_flow
+from fund_lens_etl.flows.maryland_gold_transformation_flow import (
+    maryland_gold_transformation_flow,
+)
 from fund_lens_etl.flows.maryland_ingestion_flow import (
     maryland_bronze_ingestion_flow,
     maryland_daily_ingestion_flow,
@@ -23,4 +26,6 @@ __all__ = [
     "maryland_silver_transformation_flow",
     "maryland_silver_incremental_flow",
     "maryland_silver_contributions_only_flow",
+    # Maryland gold flows
+    "maryland_gold_transformation_flow",
 ]
